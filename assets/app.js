c298c333f1d30239e8ca7d22a7b5ea0a74fd0efe
John Mateer <jj.mateer@live.com>
2019-07-23 23:09:59 -0700

    Added an on click funtion for the submit button that takes the value of all the form inputs, trims them down, and puts them in the variables that I created above. Using those variables, I pushed all of the information to firebase as a child and used a database reference that activates when a child is added and appends the information to the corresponding data cells in the table.

--- assets/app.js
+++ assets/app.js
@@ -3,19 +3,59 @@ var firebaseConfig = {
     authDomain: "trainscheduler-9e1d5.firebaseapp.com",
     databaseURL: "https://trainscheduler-9e1d5.firebaseio.com",
     projectId: "trainscheduler-9e1d5",
-    storageBucket: "",
+    storageBucket: "trainscheduler-9e1d5.appspot.com",
     messagingSenderId: "322192537019",
     appId: "1:322192537019:web:3fc8a8a4e4081b64"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
+var database = firebase.database();
+// Train Name
+// Destination 
+// First Train Time -- in military time
+// Frequency -- in minutes
+// Code this app to calculate when the next train will arrive; this should be relative to the current time.
+// Users from many different machines must be able to view same train times.
+// Styling and theme are completely up to you. Get Creative!
 
-  var trainName;
-  var destination;
-  var frequency;
-  var nextArrival;
-  var minutesAway;
+var trainName;
+var destination;
+var firstTrainTime;
+var frequency;
 
-  
+var nextArrival;
+var minutesAway;
+
+
+database.ref().on("child_added", function (snapshot) {
+    console.log(snapshot.val())
+    child = snapshot.val();
+
+    $("<tr><th>" + child.trainName + "</th><td>" + child.destination + "</td><td>" + 
+    child.firstTrainTime + "</td><td>" + child.frequency + "</td><td>"+ child.nextArrival + 
+    "</td><td>" + child.minutesAway + "</td></tr>").appendTo("tbody");
+
+
+// , function (errorObject) {
+//     console.log("error " + errorObject.code);
+// 
+});
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    trainName = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrainTime = $("#firstTrainTime").val().trim();
+    frequency = $("#frequency").val().trim();
+
+    console.log(trainName.valueOf());
+
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency
+    })
+});
+
+
